630dd46e29d1420c426a4abae19b57b83397bc21
Nitza Alfinas <nitzaalfinas@gmail.com>
2021-02-24 06:40:49 +0700

    Create server.rb

---
+++ server.rb
@@ -0,0 +1,34 @@
+require "socket"
+class Client
+  def initialize( server )
+    @server = server
+    @request = nil
+    @response = nil
+    listen
+    send
+    @request.join
+    @response.join
+  end
+
+  def listen
+    @response = Thread.new do
+      loop {
+        msg = @server.gets.chomp
+        puts "#{msg}"
+      }
+    end
+  end
+
+  def send
+    puts "Enter the username:"
+    @request = Thread.new do
+      loop {
+        msg = $stdin.gets.chomp
+        @server.puts( msg )
+      }
+    end
+  end
+end
+
+server = TCPSocket.open( "127.0.0.1", 9832 )
+Client.new( server )
